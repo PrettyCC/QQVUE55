1cb966a23b418786f4fb6639b6902a33383796f7
PrettyCC <1874152706@qq,com>
2019-04-09 19:59:41 +0800

    121

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ const cors=require('cors');
 const session=require('express-session');
 //2.创建连接池
 var pool=mysql.createPool({
-    host:'127.0.0.1',
-    port:3306,
-    user:'root',
-    password:'',
-    database:'music',
-    connectionLimit:20
+    // host:'127.0.0.1',
+    // port:3306,
+    // user:'root',
+    
+    // password:'',
+    // database:'music',
+    host     : process.env.MYSQL_HOST,
+    port     : process.env.MYSQL_PORT,
+    user     : process.env.ACCESSKEY,
+    password : process.env.SECRETKEY,
+    database : 'app_' + process.env.APPNAME,
+    connectionLimit:3
 });
 //创建express对象+
 var server=express();
